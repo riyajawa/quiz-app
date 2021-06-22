589ecee6985ea97f477828765811fdf84a07da1f
riyajawa <riya1119.cse19@chitkara.edu.in>
2021-06-22 19:31:42 +0530

    Updates

---
+++ precache-manifest.45f07a4c5f0e11e6898a03884eff9a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68929738a0f2ac6649e6cb65b3f6a9f",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "3495c7886c00d91bb3ba",
+    "url": "/quiz-app/static/css/main.26ad2c9b.chunk.css"
+  },
+  {
+    "revision": "9e16d95586e6219c5de0",
+    "url": "/quiz-app/static/js/2.ce6200f2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/quiz-app/static/js/2.ce6200f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3495c7886c00d91bb3ba",
+    "url": "/quiz-app/static/js/main.e6e496d9.chunk.js"
+  },
+  {
+    "revision": "dd430ebde0356d1a798f",
+    "url": "/quiz-app/static/js/runtime-main.34bd227c.js"
+  }
+]);
